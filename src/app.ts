60e6bc2687b0c38f88b20c9d9b28b759ba2cfa4a
Satria-WS <extencezone02@gmail.com>
2025-01-20 20:41:20 +0700

    fixing index router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import config from 'config';
-import express from 'express';
+import express, { Router ,Request, Response,} from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { initRoutes } from './routes';
 
-const app = express();
 
+const app = express();
+const indexRouter = Router();
 
 const limiter = rateLimit({
   windowMs: config.get('rateLimit.windowMs'),
@@ -26,7 +27,11 @@ app.use(cors({ origin: config.get('cors.origin'), credentials: config.get('cors.
 app.use(cookieParser());
 
 // route
-initRoutes(app);
+ initRoutes(app);
+
+// app.use('/api/', indexRouter.get('/', (req: Request, res: Response) => {
+//   res.send('app is working');
+// }))
 // port
 const port: number = config.get('port') || 9000;
 
