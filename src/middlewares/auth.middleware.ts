f965e7536417b09f0c11f47ac860e391dd668cd7
Satria-WS <extencezone02@gmail.com>
2025-01-31 00:01:26 +0700

    refactor auth middleware to streamline authorization checks and improve error handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,16 +7,16 @@ import prisma from '../utils/prisma';
 export const authMiddleware = async (req: RequestModified, res: Response, next: NextFunction) => {
   const Authorization = req.cookies['Authorization'] ||
     (req.header('Authorization') ? req.header('Authorization')?.split('Bearer ')[1] : null);
+
+  if (!Authorization || Authorization === 'undefined') {
+    return next();
+  }
+
   try {
-    if (Authorization) {
-      if (Authorization === 'undefined') {
-        next();
-      }
-      const verification = verify(Authorization, config.get('secretKey')) as DataStoredInToken;
-      const findUser = await prisma.user.findFirst({ where: { id: verification._id } });
-      if (findUser) {
-        req.user = findUser;
-      }
+    const verification = verify(Authorization, config.get('secretKey')) as DataStoredInToken;
+    const findUser = await prisma.user.findFirst({ where: { id: verification._id } });
+    if (findUser) {
+      req.user = findUser;
     }
     next();
   } catch (error) {
